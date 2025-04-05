9d303e045a731b107875608be1ee7008f0ad144d
openheimer9 <147526211+openheimer9@users.noreply.github.com>
2025-04-05 16:41:04 +0530

    Add files via upload

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: process.env.REACT_APP_API_URL || 'https://back-end-three-ivory.vercel.app',
+      target: process.env.REACT_APP_API_URL || 'http://localhost:5000',
       changeOrigin: true,
     })
   );
